1e1febbd96bbb7cd69f07ef9b182f6ca701e6a87
Otto Sanchez <otto.sancheza@gmail.com>
2019-08-14 22:17:32 -0500

    Complete customizing tests

--- server/schema.sql
+++ server/schema.sql
@@ -8,19 +8,6 @@ USE chat;
 
 /* Create other tables and define schemas for them here! */
 
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE users (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
-
--- ---
--- Table 'rooms'
---
--- ---
-
 DROP TABLE IF EXISTS `rooms`;
 
 CREATE TABLE `rooms` (
@@ -32,15 +19,14 @@ CREATE TABLE `rooms` (
 -- ---
 -- Table 'messages'
 --
--- ---
 
 DROP TABLE IF EXISTS `messages`;
 
 CREATE TABLE `messages` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
   `message` VARCHAR(255) NOT NULL,
-  `createdAt` DATETIME NOT NULL,
-  `room_id` INTEGER NOT NULL,
+  `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `room_name` VARCHAR(255) NOT NULL,
   `user_id` INTEGER NOT NULL,
   PRIMARY KEY (`id`)
 );
@@ -49,7 +35,6 @@ CREATE TABLE `messages` (
 -- Foreign Keys
 -- ---
 
-ALTER TABLE `messages` ADD FOREIGN KEY (room_id) REFERENCES `rooms` (`id`);
 ALTER TABLE `messages` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
 
 -- ---
@@ -68,11 +53,5 @@ ALTER TABLE `messages` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
 -- ('','');
 -- INSERT INTO `rooms` (`id`,`roomname`) VALUES
 -- ('','');
--- INSERT INTO `messages` (`id`,`message`,`createdAt`,`room_id`,`user_id`) VALUES
+-- INSERT INTO `messages` (`id`,`message`,`createdAt`,`room_name`,`user_id`) VALUES
 -- ('','','','','');
-
-
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
-
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -10,13 +10,13 @@ describe('Persistent Node Chat Server', function() {
 
   beforeEach(function(done) {
     dbConnection = mysql.createConnection({
-      user: 'student',
-      password: 'student',
+      user: 'root',
+      password: '',
       database: 'chat'
     });
     dbConnection.connect();
 
-       var tablename = ""; // TODO: fill this out
+    var tablename = "messages"; // TODO: fill this out // Changed to messages
 
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
@@ -49,15 +49,15 @@ describe('Persistent Node Chat Server', function() {
 
         // TODO: You might have to change this test to get all the data from
         // your message table, since this is schema-dependent.
-        var queryString = 'SELECT * FROM messages';
-        var queryArgs = [];
+        var queryString = 'SELECT * FROM messages WHERE `id`= (SELECT `id` FROM users WHERE name = ?)';
+        var queryArgs = ['Valjean'];
 
         dbConnection.query(queryString, queryArgs, function(err, results) {
           // Should have one result:
           expect(results.length).to.equal(1);
 
-          // TODO: If you don't have a column named text, change this test.
-          expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
+          // TODO: If you don't have a column named text, change this test. ** changed to message
+          expect(results[0].message).to.equal('In mercy\'s name, three days is all I need.');
 
           done();
         });
@@ -67,8 +67,8 @@ describe('Persistent Node Chat Server', function() {
 
   it('Should output all messages from the DB', function(done) {
     // Let's insert a message into the db
-       var queryString = "";
-       var queryArgs = [];
+    var queryString = "INSERT INTO messages(message, room_name, user_id) VALUES (?, ?, ?)";
+    var queryArgs = ['Men like you can never change!', 'main', 1];
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
@@ -80,8 +80,8 @@ describe('Persistent Node Chat Server', function() {
       // the message we just inserted:
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
         var messageLog = JSON.parse(body);
-        expect(messageLog[0].text).to.equal('Men like you can never change!');
-        expect(messageLog[0].roomname).to.equal('main');
+        expect(messageLog[0].message).to.equal('Men like you can never change!');
+        expect(messageLog[0].room_name).to.equal('main');
         done();
       });
     });
